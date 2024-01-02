d775cc3844d0925d6d05dc62102699662fb8362e
Agrata Patel <agratapatel@gmail.com>
2024-01-02 20:19:51 +0530

    week 1 day 1 assignments

---
+++ week-1/offline-class-1/level-1/callbacks.js
@@ -0,0 +1,24 @@
+function returningSum(num1, num2, fnToCall) {
+  let result = num1 + num2;
+  fnToCall(result);
+}
+
+function displayResult(data) {
+    console.log("Result:", data);
+}
+
+function displayResultPassive(data) {
+    console.log("Result:", data);
+}
+
+const answer = returningSum(1, 2, displayResult);
+// displayResult(answer);
+// callback function is a function that is passed as an argument to another function
+// recursion is when a function calls itself, callback is when you call one function as an argument to another function
+
+function greet() {
+    console.log("Hello World");
+}
+
+setTimeout(greet, 2 * 1000, answer);
+// setTimeout defines after how many milliseconds the function should be called
